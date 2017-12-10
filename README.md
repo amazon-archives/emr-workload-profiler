dc585c07fa77ed1a2968a19835bea9bd5db2fcf0
Marc Beitchman <beitchma@amazon.com>
2017-12-10 01:47:16 -0800

    Add initial profiler classes
    This change makes the profiler class an interface. The reason to add
    the interface is to allow for polymorphic dispatch of one or many
    profilers to collect metrics from many parts of the application stack.
    
    The workload profiler will eventually support all application types
    on EMR.
    
    Pull requests are always welcome!

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Amazon EMR Workload Profiler
 ## License
 
 This library is licensed under the Apache 2.0 License. 
+
+
--- src/test/java/com/aws/emr/profiler/core/SparkProfilerTest.java
+++ src/test/java/com/aws/emr/profiler/core/SparkProfilerTest.java
@@ -16,6 +16,12 @@
 
 package com.aws.emr.profiler.core;
 
-public interface Profiler {
-     void profile();
+import org.testng.annotations.Test;
+
+public class SparkProfilerTest {
+    @Test(expectedExceptions = NotImplementedException.class)
+    public void notImplementedTest() {
+        Profiler p = new SparkProfiler();
+        p.profile();
+    }
 }
